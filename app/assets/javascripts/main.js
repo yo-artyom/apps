161bb371f87217b6913b23ff0131656b2008930b
q3pp <q3pp@yandex.ru>
2015-05-08 13:53:15 +0300

    edit js

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,23 +1,22 @@
 var ready;
+var selected_item;
 ready = function() {
 
     //open
 
     $('#items').find('ul a').on('click', function(event){
         event.preventDefault();
-        var selected_item = $(this).data('type');
+        selected_item = $(this).data('type');
         $('body').addClass('overflow-hidden');
         $('.info-panel-'+selected_item).addClass('is-visible');
-        $('#items').addClass('slide-out');
+        $('.overlay').addClass('on');
     });
 
     //close
-    $('#items').find('ul a').on('click', function(event){
+    $(document).on('click', '.overlay, .close', function(event){
         event.preventDefault();
-        var si = $('#items').find('ul a').data(type);
-        //if( $(event.target).is('.info-panel-'+si) || $(event.target).is('.info-panel-close') ) {
-
-        $('.info-panel-'+si).removeClass('is-visible');
+        $('.overlay').removeClass('on');
+        $('.info-panel-'+selected_item).removeClass('is-visible');
 
         //}
     });
