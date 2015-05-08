4cb817f1bf348e8dff8458ac21eb6639ae3dbeb5
q3pp <q3pp@yandex.ru>
2015-05-08 07:59:13 +0300

    edit js

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -6,17 +6,20 @@ ready = function() {
     $('#items').find('ul a').on('click', function(event){
         event.preventDefault();
         var selected_item = $(this).data('type');
+        $('body').addClass('overflow-hidden');
         $('.info-panel-'+selected_item).addClass('is-visible');
+        $('#items').addClass('slide-out');
     });
 
     //close
-    $('.items').on('click', function(event){
+    $('#items').find('ul a').on('click', function(event){
+        event.preventDefault();
         var si = $('#items').find('ul a').data(type);
+        //if( $(event.target).is('.info-panel-'+si) || $(event.target).is('.info-panel-close') ) {
 
-        if( $(event.target).is('.info-panel-'+si) || $(event.target).is('.info-panel-close') ) {
-            $('.info-panel-'+si).removeClass('is-visible');
-            event.preventDefault();
-        }
+        $('.info-panel-'+si).removeClass('is-visible');
+
+        //}
     });
 };
  $(document).ready(ready)
