e66e56f51952b542fae185cd9214709f1a1ceebe
q3pp <q3pp@yandex.ru>
2015-05-15 17:02:19 +0300

    add rating

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,9 @@ class Item < ActiveRecord::Base
   APP_TYPE = %w( Game App )
   validates :name, :desc, presence: true
   validates :app_type, inclusion: APP_TYPE
-  validates :rating, presence: true
+  validates :rating,
+            :numericality => { :greater_than => 0, :less_than_or_equal_to => 5 },
+            presence: true
 end
 
 
