85eca5e5122a9dd764d2ec1e8b5fcfd15402d256
q3pp <q3pp@yandex.ru>
2015-06-12 16:53:42 +0300

    finish paperclip

--- config/deploy.rb
+++ config/deploy.rb
@@ -106,15 +106,6 @@ namespace :deploy do
     run "[ -f #{unicorn_pid} ] && kill -USR2 `cat #{unicorn_pid}` || #{unicorn_start_cmd}"
   end
 
-  desc "build missing paperclip styles"
-  task :build_missing_paperclip_styles do
-    on roles(:app) do
-      within release_path do
-        with rails_env: fetch(:rails_env) do
-          execute :rake, "paperclip:refresh:missing_styles"
-        end
-      end
-    end
-  end
+
 
 end
