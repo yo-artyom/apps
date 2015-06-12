f95f7f1e014ec325bfca16bf38487327a6edb9d1
q3pp <q3pp@yandex.ru>
2015-06-12 16:24:58 +0300

    finish paperclip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,7 @@ Rails.application.configure do
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
-
+  Paperclip.options[:log] = false
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = true
 
