eee8ec8edd865a90df6e7b7a6ae8e8a9184cc29b
q3pp <q3pp@yandex.ru>
2015-06-12 16:54:48 +0300

    finish paperclip

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,7 @@ class Item < ActiveRecord::Base
 
   has_attached_file :image, :styles => {:small => "80x80", :medium => "300x300>",  :thumb => "100x100>" },
                     :url => "/:attachment/:id/:style/:basename.:extension",
-                    :path => ":rails_root/public/:attachment/:id/:style/:basename.:extension"
+                    :path => "/:attachment/:id/:style/:basename.:extension"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   validates :name, :desc, presence: true
