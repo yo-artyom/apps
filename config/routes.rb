d07577f5532170ecdb6d1a872cfe3c9a3039b12d
q3pp <q3pp@yandex.ru>
2015-06-23 18:12:02 +0300

    resize star

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   match '/main',   to: 'main_pages#index',      via: 'get'
   match '/game',   to: 'main_pages#game_page',      via: 'get'
   match '/app',    to: 'main_pages#app_page',      via: 'get'
-  match '/test',   to: 'main_pages#test', via: 'get'
-  root 'main_pages#game_page'
+  match '/',   to: 'main_pages#test', via: 'get'
+  root 'main_pages#test'
 
 end
