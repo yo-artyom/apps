20de48c6dcd167e09b0b99dafda8c79f486585f5
q3pp <q3pp@yandex.ru>
2015-07-03 15:53:50 +0300

    prevent scroll v11

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -9,7 +9,7 @@ ready = function() {
 
     //open
     $('#items').find('.item-info').on('click', function(event){
-        $('body').on('touchmove', function(e){
+        $('.overlay').on('touchmove', function(e){
             e.preventDefault();
             e.stopPropagation();
         });
