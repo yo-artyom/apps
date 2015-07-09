8d8184c4c08bf1c3b910f1a9f800a0c089f6005a
q3pp <q3pp@yandex.ru>
2015-07-09 20:25:51 +0300

    work

--- app/controllers/main_pages_controller.rb
+++ app/controllers/main_pages_controller.rb
@@ -9,15 +9,14 @@ class MainPagesController < ApplicationController
   end
 
   def main
-    device =request.env['HTTP_USER_AGENT'].downcase
-    if device.match(/android|iphone|ipad/)
 
-      @apps =  Item.only_app.where( "#{detect_browser}_link != '' ")
-      @games = Item.only_game.where("#{detect_browser}_link != '' ")
-    else
+    #if request.env['HTTP_USER_AGENT'].downcase.match(/android|iphone|ipad/)
+     # @apps =  Item.only_app.where( "#{detect_browser}_link != '' ")
+      #@games = Item.only_game.where("#{detect_browser}_link != '' ")
+    #else
       @apps = Item.only_app
       @games = Item.only_game
-    end
+    #end
 
   end
 
