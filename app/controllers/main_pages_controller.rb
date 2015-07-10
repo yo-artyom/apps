fbf8ea9a3e4adada2cc666e5d26fba8d8536054f
q3pp <q3pp@yandex.ru>
2015-07-10 14:09:56 +0300

    add detect os

--- app/controllers/main_pages_controller.rb
+++ app/controllers/main_pages_controller.rb
@@ -9,19 +9,32 @@ class MainPagesController < ApplicationController
   end
 
   def main
-
-    #if request.env['HTTP_USER_AGENT'].downcase.match(/android|iphone|ipad/)
-     # @apps =  Item.only_app.where( "#{detect_browser}_link != '' ")
-      #@games = Item.only_game.where("#{detect_browser}_link != '' ")
-    #else
+    device = request.env['HTTP_USER_AGENT'].downcase
+    if device.match(/android|iphone|ipad/)
+      @apps =  Item.only_app.where( "#{detect_browser}_link != '' ")
+      @games = Item.only_game.where("#{detect_browser}_link != '' ")
+    else
       @apps = Item.only_app
       @games = Item.only_game
-    #end
+    end
 
   end
 
   def test
     @apps =  Item.only_app
     @games = Item.only_game
+  end
+
+  def detect_browser
+    user_agent =  request.env['HTTP_USER_AGENT'].downcase
+    if user_agent.index('ipad')
+      'ipad'
+    elsif user_agent.index('iphone')
+      'iphone'
+    elsif user_agent.index('android')
+      'chrome'
+    else
+      'unknown'
+    end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,4 @@
 module ApplicationHelper
-  def detect_browser
-    user_agent =  request.env['HTTP_USER_AGENT'].downcase
-      if user_agent.index('ipad')
-        'ipad'
-      elsif user_agent.index('iphone')
-        'iphone'
-      elsif user_agent.index('android')
-        'chrome'
-      else
-        'unknown'
-      end
-    end
+
 end
 
