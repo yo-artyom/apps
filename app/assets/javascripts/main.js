299ee9dc911456a3598168ec34d5cd3ecac7f1dd
q3pp <q3pp@yandex.ru>
2015-07-20 14:27:24 +0300

    dewewsw

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -7,25 +7,20 @@ ready = function() {
     // $('.col .apps:even').css('background-color','#DEE5EB');
 
     //open
-    $('#items').find('.item-info').on('click', function(event){
+    $('.items').find('.item-info').on('click', function(event){
         selected_item = $(this).data('type');
         $('html , body').css({'overflow':'hidden'});
-        //$('body').addClass('overflow-hidden');
         $('.info-panel-'+selected_item).addClass('is-visible');
         $('.overlay').fadeIn(400);
         $('.close').addClass('is-visible ');
     });
 
-    $('.nav-border').on('click', function(){
-    });
-
     //close
     $(document).on('click', '.close, .overlay ', function(event){
         event.preventDefault();
-        $('body').removeClass('overflow-hidden');
         $('html').css({'overflow':'scroll'});
         $('.info-panel-'+selected_item).removeClass('is-visible');
-        $('.overlay').fadeOut(300);
+        $('.overlay').fadeOut(400);
         $('.close').removeClass('is-visible');
     });
 
